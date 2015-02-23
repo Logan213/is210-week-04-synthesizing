7c5a9bb3363f365585996dbd002c9506095fb9b1
Logan Thomson <logan.t.music@gmail.com>
2015-02-23 00:01:48 +0000

    Made changes to task 3, still having a problem with inf integer

--- task_03.py
+++ task_03.py
@@ -9,58 +9,64 @@ PRINCIPAL = raw_input('What is the amount being borrowed? ')
 PRINCIPAL = int(PRINCIPAL)
 TERM = raw_input('What is the term of the loan? ')
 TERM = int(TERM)
-QUAL = raw_input('Are you pre-qualified for this loan? ')
+QUAL_INPUT = raw_input('Are you pre-qualified for this loan? ')
+QUAL = QUAL_INPUT[:1].lower() == 'y'
 RATE = 0
 
 if PRINCIPAL <= 199999:
     if 1 <= TERM <= 15:
-        if QUAL == 'Yes':
+        if QUAL:
             RATE = decimal.Decimal('.0363')
         else:
             RATE = decimal.Decimal('.0465')
     elif 16 <= TERM <= 20:
-        if QUAL == 'Yes':
+        if QUAL:
             RATE = decimal.Decimal('.0404')
         else:
             RATE = decimal.Decimal('.0498')
     elif 21 <= TERM <= 30:
-        if QUAL == 'Yes':
+        if QUAL:
             RATE = decimal.Decimal('.0577')
         else:
             RATE = decimal.Decimal('.0639')
 elif 200000 <= PRINCIPAL <= 999999:
     if 1 <= TERM <= 15:
-        if QUAL == 'Yes':
+        if QUAL:
             RATE = decimal.Decimal('.0302')
         else:
             RATE = decimal.Decimal('.0398')
     elif 16 <= TERM <= 20:
-        if QUAL == 'Yes':
+        if QUAL:
             RATE = decimal.Decimal('.0327')
         else:
             RATE = decimal.Decimal('.0408')
     elif 21 <= TERM <= 30:
-        if QUAL == 'Yes':
+        if QUAL:
             RATE = decimal.Decimal('.0466')
         else:
-            RATE = 0
+            RATE is None
 else:
     if 1 <= TERM <= 15:
-        if QUAL == 'Yes':
+        if QUAL:
             RATE = decimal.Decimal('.0205')
         else:
             RATE = 0
-    if 16 <= TERM <= 20:
-        if QUAL == 'Yes':
+    elif 16 <= TERM <= 20:
+        if QUAL:
             RATE = decimal.Decimal('.0262')
         else:
-            RATE = 0
+            RATE is None
+    else:
+        if QUAL:
+            RATE is None
+        else:
+            RATE is None
 
-if RATE == 0:
-    TOTAL = None
+if RATE is None:
+    TOTAL = None 
 else:
     TOTAL = (PRINCIPAL * (1 + (RATE / 12))) ** (12 * TERM)
-    TOTAL = int(round(TOTAL))
+    TOTAL = round(TOTAL)
 
 LINE = '_' * 60
 
